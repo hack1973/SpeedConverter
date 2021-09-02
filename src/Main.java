c8f92e0a828ae1ab9e497f5f8390a3fd13e95814
Michael Hacker <hack1973@swbell.net>
2021-09-01 22:26:03 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(SpeedConverter.toMilesPerHour(75.114));
+
+        SpeedConverter.printConversion(75.114);
+    }
+}
